37de5e81be881a2fc7393517ba9542a2aed3167a
amainooti <amainosamuel1@gmail.com>
2024-10-13 02:32:48 +0100

    socket io implementation for the websocket gateway completed

--- src/gateway/gateway.ts
+++ src/gateway/gateway.ts
@@ -1,13 +1,32 @@
+import { OnModuleInit } from '@nestjs/common';
 import {
   MessageBody,
   SubscribeMessage,
   WebSocketGateway,
+  WebSocketServer,
 } from '@nestjs/websockets';
 
+import { Server } from 'socket.io';
+
 @WebSocketGateway()
-export class Mygateway {
+export class Mygateway implements OnModuleInit {
+  @WebSocketServer()
+  server: Server;
+
+  onModuleInit() {
+    this.server.on('connection', (socket) => {
+      console.log(socket.id);
+      console.log('connected');
+    });
+  }
+
   @SubscribeMessage('newMessage')
   onMessage(@MessageBody() body: any) {
     console.log(body);
+
+    this.server.emit('onMessage', {
+      msg: 'New Message',
+      content: body,
+    });
   }
 }
